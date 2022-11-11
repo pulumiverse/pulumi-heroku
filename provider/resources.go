f101ed329d50e143f20fb8e7751e213ced00fd4f
Marcel Arns <marcel.arns@moneymeets.com>
2022-11-11 08:53:53 +0100

    chore(deps): bump depdencies + update to terraform-provider-heroku v5

--- provider/resources.go
+++ provider/resources.go
@@ -18,7 +18,7 @@ import (
 	"fmt"
 	"path/filepath"
 
-	"github.com/heroku/terraform-provider-heroku/v4/heroku"
+	"github.com/heroku/terraform-provider-heroku/v5/heroku"
 	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge"
 	shim "github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfshim"
 	shimv2 "github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfshim/sdk-v2"
